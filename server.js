4a3317e9730a2ee3b7bd60176d9fa2a9a4b85f34
KaiTang26 <16kt16@queensu.ca>
2019-04-05 17:12:53 -0400

    user fetch get api call

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+const fetch = require('node-fetch');
 
-app.get('/',(req, res)=>res.send('Hello Kai'));
+app.get('/',(req, res)=>{
+    const apiUrl = 'http://xkcd.com/info.0.json';
+
+    fetch(apiUrl)
+        .then(apires => apires.json())
+        .then(data => {
+            res.json(data);
+        });
+});
 
 const port = process.env.PORT || 8080;
 
