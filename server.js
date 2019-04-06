09c52c6bfe091d34a24d62563817f281ddb30bf0
KaiTang26 <16kt16@queensu.ca>
2019-04-06 08:23:19 -0400

    install react.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
+//const bodyParser = require('body-parser');
+//const fetch = require('node-fetch');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -14,69 +14,34 @@ mongoose
     .then(()=> console.log('MonogoDB Connected'))
     .catch(err=> console.log(err));
 
-//Body parser middleware
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
 
 const Count = require('./models/Count');
+
+const ApiHandler = require('./handler/ApiHandler');
+
+const validator = require('./validation/validator');
+
+
 
 app.get('/',(req, res)=>{
     const apiUrl = 'http://xkcd.com/info.0.json';
     ApiHandler(apiUrl, res, Count)  
 });
 
+
+
 app.get('/:num',(req, res)=>{
-
     const num = req.params.num;
-    console.log(num);
-    const apiUrl = `https://xkcd.com/${num}/info.0.json`;
-
-    ApiHandler(apiUrl, res, Count)
-    
+    if(validator(parseInt(num))){
+        const apiUrl = `https://xkcd.com/${num}/info.0.json`;
+        ApiHandler(apiUrl, res, Count)
+    }else{
+        res.json({
+            'error':'input param wrong'
+            });
+    } 
 });
 
-function ApiHandler(url, res, Count){
-    fetch(url)
-        .then(apires => apires.json())
-        .then(data => {
-            Count.findOne({num:data.num})
-                .then(count =>{
-                    if(count){
-                        count.count = count.count+1;
-                        count.save()
-                             .then(count=>{
-                                res.json({
-                                    'num': data.num,
-                                    'title': data.title,
-                                    'year': data.year,
-                                    'month':data.month,
-                                    'day':data.day,
-                                    'img':data.img,
-                                    'count': count.count});
-                            })
-                            .catch(error => console.error(error));
-
-                    } else{
-                        const newCount = new Count({
-                            num:data.num,
-                            count: 1 });
-                         newCount.save()
-                        .then(count=>{
-                            res.json({
-                                'num': data.num,
-                                'title': data.title,
-                                'year': data.year,
-                                'month':data.month,
-                                'day':data.day,
-                                'img':data.img,
-                                'count': count.count});
-                            })
-                        .catch(error => console.error(error));
-
-                    }});
-            })
-        .catch(error => console.error(error));
-}
 
 const port = process.env.PORT || 8080;
 
---
+++ validation/validator.js
@@ -0,0 +1,10 @@
+module.exports = function validator (num){
+
+    if(num >0 && num <= 2133){
+        return true
+    }else{
+        return false
+
+    }
+
+}
