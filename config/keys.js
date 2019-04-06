498edd99b6b92a5f1c5f3fe86f3c4fa3d2baa656
KaiTang26 <16kt16@queensu.ca>
2019-04-06 00:08:33 -0400

    db has been conntected

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://cyber:cyber@cluster0-l4l7d.mongodb.net/test?retryWrites=true'
+}
--- server.js
+++ server.js
@@ -1,16 +1,82 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(()=> console.log('MonogoDB Connected'))
+    .catch(err=> console.log(err));
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const Count = require('./models/Count');
 
 app.get('/',(req, res)=>{
     const apiUrl = 'http://xkcd.com/info.0.json';
+    ApiHandler(apiUrl, res, Count)  
+});
 
-    fetch(apiUrl)
+app.get('/:num',(req, res)=>{
+
+    const num = req.params.num;
+    console.log(num);
+    const apiUrl = `https://xkcd.com/${num}/info.0.json`;
+
+    ApiHandler(apiUrl, res, Count)
+    
+});
+
+function ApiHandler(url, res, Count){
+    fetch(url)
         .then(apires => apires.json())
         .then(data => {
-            res.json(data);
-        });
-});
+            Count.findOne({num:data.num})
+                .then(count =>{
+                    if(count){
+                        count.count = count.count+1;
+                        count.save()
+                             .then(count=>{
+                                res.json({
+                                    'num': data.num,
+                                    'title': data.title,
+                                    'year': data.year,
+                                    'month':data.month,
+                                    'day':data.day,
+                                    'img':data.img,
+                                    'count': count.count});
+                            })
+                            .catch(error => console.error(error));
+
+                    } else{
+                        const newCount = new Count({
+                            num:data.num,
+                            count: 1 });
+                         newCount.save()
+                        .then(count=>{
+                            res.json({
+                                'num': data.num,
+                                'title': data.title,
+                                'year': data.year,
+                                'month':data.month,
+                                'day':data.day,
+                                'img':data.img,
+                                'count': count.count});
+                            })
+                        .catch(error => console.error(error));
+
+                    }});
+            })
+        .catch(error => console.error(error));
+}
 
 const port = process.env.PORT || 8080;
 
