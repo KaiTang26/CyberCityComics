02b3277f8c1df9db0cc6fcf8965b8fbeed5f63a7
KaiTang26 <16kt16@queensu.ca>
2019-04-06 21:25:46 -0400

    for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -49,6 +50,16 @@ app.get('/api/:num',(req, res)=>{
             });
     } 
 });
+
+//server static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*',(req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+
+}
 
 
 const port = process.env.PORT || 8080;
